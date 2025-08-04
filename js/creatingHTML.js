737ad76461763e4d86f3b78ca80212f71e5130d9
Aksi147 <akjr820@gmail.com>
2025-08-04 19:22:35 -0400

    popup HTML is created for every card. Save point before making it dynamic

--- js/creatingHTML.js
+++ js/creatingHTML.js
@@ -1,3 +1,5 @@
+// Creating HTML for cards
+
 const portfolioCards = [
   {
     dataItem: "web",
@@ -5,6 +7,12 @@ const portfolioCards = [
     imgSrc: "./assets/images/portfolio-1.jpg",
     div: "Web Development",
     h3: "Museum Website",
+    popupData: {
+      h3: "Web Project 1",
+      p1: "My first awesome website",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
   {
     dataItem: "ui",
@@ -12,6 +20,12 @@ const portfolioCards = [
     imgSrc: "./assets/images/portfolio-2.jpg",
     div: "UI Design",
     h3: "Social Media",
+    popupData: {
+      h3: "UI Project 1",
+      p1: "My first awesome UI project",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
   {
     dataItem: "web",
@@ -19,6 +33,12 @@ const portfolioCards = [
     imgSrc: "./assets/images/portfolio-3.jpg",
     div: "Web Development",
     h3: "Dropship Website",
+    popupData: {
+      h3: "Web Project 2",
+      p1: "My second awesome website",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
   {
     dataItem: "web",
@@ -26,6 +46,12 @@ const portfolioCards = [
     imgSrc: "./assets/images/portfolio-4.jpg",
     div: "Web Development",
     h3: "Food Website",
+    popupData: {
+      h3: "Web Project 3",
+      p1: "My third awesome website",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
   {
     dataItem: "app",
@@ -33,6 +59,12 @@ const portfolioCards = [
     imgSrc: "./assets/images/portfolio-5.jpg",
     div: "App Development",
     h3: "Game App",
+    popupData: {
+      h3: "App Project 1",
+      p1: "My first awesome app",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
   {
     dataItem: "app",
@@ -40,6 +72,12 @@ const portfolioCards = [
     imgSrc: "../assets/images/portfolio-6.jpg",
     div: "App Development",
     h3: "Betting App",
+    popupData: {
+      h3: "App Project 2",
+      p1: "My second awesome app",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
   {
     dataItem: "app",
@@ -47,6 +85,12 @@ const portfolioCards = [
     imgSrc: "../assets/images/portfolio-7.jpg",
     div: "App Development",
     h3: "Financing App",
+    popupData: {
+      h3: "App Project 3",
+      p1: "My third awesome app",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
   {
     dataItem: "ui",
@@ -54,6 +98,12 @@ const portfolioCards = [
     imgSrc: "./assets/images/portfolio-8.jpg",
     div: "UI Design",
     h3: "Fantastic Design",
+    popupData: {
+      h3: "UI Project 2",
+      p1: "My second awesome UI project",
+      p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, totam.",
+      p3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae?",
+    },
   },
 ];
 
@@ -86,7 +136,7 @@ for (const cardData of portfolioCards) {
   categoryDiv.innerHTML = cardData.div;
 
   //create title h3
-  const title = document.createElement("div");
+  const title = document.createElement("h3");
   title.innerHTML = cardData.h3;
 
   //build nested structure
@@ -99,3 +149,41 @@ for (const cardData of portfolioCards) {
 }
 
 portfolioGrid.appendChild(fragment);
+
+// End of dynamic card HTML with JS
+
+const popupModalsDiv = document.querySelector(".all-popup-modals");
+
+const fragmentTwo = document.createDocumentFragment();
+
+for (const popup of portfolioCards) {
+  const modalPopup = document.createElement("div");
+
+  modalPopup.innerHTML = `
+      <div id="${popup.dataOpen}" class="modal" data-animation="slideInOutTop">
+        <div class="modal-dialog">
+          <header class="modal-header">
+            <h3>${popup.popupData.h3}</h3>
+            <i class="fas fa-times" data-close></i>
+          </header>
+          <div class="modal-body">
+            <div class="img-wrapper">
+              <img src="${popup.imgSrc}" alt="portfolio img" />
+            </div>
+            <div class="text-wrapper">
+              <p><strong>${popup.popupData.p1}</strong></p>
+              <p>
+                ${popup.popupData.p2}
+              </p>
+              <p>
+                ${popup.popupData.p3}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+  fragmentTwo.appendChild(modalPopup);
+}
+
+popupModalsDiv.appendChild(fragmentTwo);
